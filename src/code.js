974157cae2be9431249c0c82512e7db228cdc358
reoya0807 <reoya0807@yahoo.co.jp>
2022-12-27 17:05:45 +0900

    0.2a 貸借対照表にあたる部分を模擬データで構成 その他デザインを整えた

--- src/code.js
+++ src/code.js
@@ -5,7 +5,7 @@ function doGet() {
    var results = (funcNameRegex).exec((this).constructor.toString());
    return (results && results.length > 1) ? results[1] : "";
 };
-  var t =HtmlService.createTemplateFromFile('index');
+  var t = HtmlService.createTemplateFromFile('index');
   var sheet = SpreadsheetApp
     .openById('1K7EG0h0Qz5qcIC5SgQ73gGKgRNHfu0mgmCqQ2HeuXNI')
     .getActiveSheet();
@@ -15,5 +15,23 @@ function doGet() {
     .getValues();
   // デバッグ用の型出力
   console.log(t.data[0][0].getName());
+
+  // テスト用データ
+  t.assetData = [
+    ["現金", 500],
+    ["Suica", 5000],
+    ["Paypay", 10000],
+    ["ゆうちょ", 1000000],
+    ["みずほ銀行", 100000]
+  ];
+  t.debtData = [
+    ["クレジットカード", 50000]
+  ];
+  t.reserveFundData = [
+    ["美容積立資金", 700000],
+    ["旅行積立資金", 50000],
+    ["引越積立資金", 300000],
+    ["プレゼント積立資金", 15500]
+  ];
   return t.evaluate();
 }
